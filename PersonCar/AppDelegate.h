24f123cae999afb22a01a3368406892a2f058d5b
Paul Solt <PaulSolt@gmail.com>
2019-04-10 11:28:30 -0400

    Initial Commit

---
+++ PersonCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonCar
+//
+//  Created by Paul Solt on 4/10/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
